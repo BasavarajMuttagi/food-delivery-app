913988f6771d1c9b86b91ac86993d3855a98eb82
BasavarajMuttagi <basavaraj2770@gmail.com>
2024-04-15 17:40:41 +0530

    add empty cart state

--- src/components/Cart.tsx
+++ src/components/Cart.tsx
@@ -1,12 +1,28 @@
 import OrderSummary from "./OrderSummary";
 import useFoodStore from "../../store";
 import MenuItemCard from "./MenuItemCard";
+import { Basket } from "@phosphor-icons/react";
 
 function Cart() {
   const { itemsArray } = useFoodStore();
   const sortedItems = itemsArray.slice().sort((a, b) => {
     return a.name.localeCompare(b.name);
   });
+
+  if (itemsArray.length == 0) {
+    return (
+      <div className="flex flex-col justify-center items-center space-y-20 pb-36 p-2 w-full sm:w-[60%]">
+        <div className="flex space-x-4 items-center">
+          <Basket
+            size={40}
+            weight="fill"
+            className="text-green-400 hover:animate-spin duration-300"
+          />{" "}
+          <span>Cart is empty</span>
+        </div>
+      </div>
+    );
+  }
   return (
     <div className="space-y-20 pb-36 p-2 w-full sm:w-[60%]">
       <div className="space-y-4">
@@ -23,8 +39,8 @@ function Cart() {
         ))}
       </div>
 
-      <div className="border-b"></div>
-      <OrderSummary />
+      {itemsArray.length > 0 && <div className="border-b"></div>}
+      {itemsArray.length > 0 && <OrderSummary />}
     </div>
   );
 }
