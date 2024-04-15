85c59fbd61a1eb323c4dffeea41631cc425bc82f
BasavarajMuttagi <basavaraj2770@gmail.com>
2024-04-15 17:07:36 +0530

    refactor deals

--- src/components/DealCard.tsx
+++ src/components/DealCard.tsx
@@ -1,20 +1,36 @@
+import { twMerge } from "tailwind-merge";
+
 type dealProps = {
   label: string;
   code: string;
   description: string;
+  isActive: boolean;
 };
 
-function DealCard({ label, code, description }: dealProps) {
+function DealCard({ label, code, description, isActive }: dealProps) {
   return (
     <div className="pb-10 w-full border bg-white rounded-lg">
-      <div className="font-extrabold bg-red-400 text-white text-center p-2 rounded-t-lg">
+      <div
+        className={twMerge(
+          "font-extrabold bg-red-400 text-white text-center p-2 rounded-t-lg",
+          isActive ? "" : "bg-slate-400"
+        )}
+      >
         {label}
       </div>
       <div className="flex justify-between items-center p-4 space-x-4">
         <div className="p-2  rounded w-full tracking-[0.5rem] font-bold text-2xl text-center">
           {code}
         </div>
-        <button className="p-2 rounded bordertext-2xl">Apply</button>
+        <button
+          disabled={!isActive}
+          className={twMerge(
+            "p-2 rounded bordertext-2xl",
+            isActive ? "" : "cursor-not-allowed"
+          )}
+        >
+          Apply
+        </button>
       </div>
       <div className="tracking-wider text-sm font-medium bg-white text-slate-800/50 text-center p-2 rounded-b-lg">
         {description}
--- src/components/Deals.tsx
+++ src/components/Deals.tsx
@@ -1,23 +1,47 @@
 import DealCard from "./DealCard";
 
+type Deal = {
+  isActive: boolean;
+  label: string;
+  code: string;
+  description: string;
+};
+
+const deals: Deal[] = [
+  {
+    isActive: true,
+    label: "FLAT OFF",
+    code: "FLAT125",
+    description:
+      "Use code FLAT125 and get flat ₹125 off on order above ₹999. T&C Apply",
+  },
+  {
+    isActive: true,
+    label: "20% OFF",
+    code: "SPECIALS",
+    description:
+      "Use code SPECIALS and get 20% off on order above ₹200. T&C Apply",
+  },
+  {
+    isActive: false,
+    label: "PhonePe",
+    code: "No Code",
+    description:
+      "Pay Using PhonePe and stand a change win 30% of the order value. T&C Apply",
+  },
+];
 function Deals() {
   return (
     <div className="space-y-10 p-2 w-full sm:w-[60%]">
-      <DealCard
-        label="FLAT OFF"
-        code="FLAT125"
-        description="Use code FLAT125 and get flat ₹125 off on order above ₹999. T&C Apply"
-      />
-      <DealCard
-        label="20% OFF"
-        code="SPECIALS"
-        description="Use code SPECIALS and get 20% off on order above ₹200. T&C Apply"
-      />
-      <DealCard
-        label="PhonePe"
-        code="No Code"
-        description="Pay Using PhonePe and stand a change win 30% of the order value. T&C Apply"
-      />
+      {deals.map((deal) => (
+        <DealCard
+          key={deal.code}
+          isActive={deal.isActive}
+          label={deal.label}
+          code={deal.code}
+          description={deal.description}
+        />
+      ))}
     </div>
   );
 }
